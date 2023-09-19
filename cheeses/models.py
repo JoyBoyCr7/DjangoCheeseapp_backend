c367d9c0c36581e3a070ea679d98404b085ff94e
JoyBoyCr7 <Rnyongkah@gmail.com>
2023-09-19 14:13:34 -0500

    caps change

--- cheeses/models.py
+++ cheeses/models.py
@@ -5,5 +5,5 @@ from django.db import models
 # Create your models here.
 class Cheese(models.Model):
     name = models.CharField(max_length=100)
-    origin_country = models.CharField(max_length=100)
+    origin_Country = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
